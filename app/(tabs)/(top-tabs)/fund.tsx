a946ba9747eff2b688678c144e6fcb6cddcdde95
Patrick <1217303583@qq.com>
2025-02-03 22:20:55 +0800

    Refactor Combine and Fund screens to use SafeAreaView for improved layout and styling

--- app/(tabs)/(top-tabs)/fund.tsx
+++ app/(tabs)/(top-tabs)/fund.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Fund = () => {
   return (
-    <View>
-      <Text>Fund</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-gray-50">
+      <View className='ml-8 mr-8 mt-4'>
+        <Text>Fund</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
